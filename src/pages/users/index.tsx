e36e202435af4d3ba59852f03ad5b5d8114b473b
Eduardo-SO <eduardo.sool07@gmail.com>
2021-04-15 23:14:18 -0300

    Add users page initial structure

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,129 @@
+import { Box, Button, Checkbox, Flex, Heading,Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex justify="space-between" align="center" mb="8">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo usuário
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th w="8" px="6" color="gray.300">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Eduardo Souza</Text>
+                    <Text fontSize="sm" color="gray.300">eduardo.sool07@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text>15 de Março, 2021</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Eduardo Souza</Text>
+                    <Text fontSize="sm" color="gray.300">eduardo.sool07@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text>15 de Março, 2021</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Eduardo Souza</Text>
+                    <Text fontSize="sm" color="gray.300">eduardo.sool07@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text>15 de Março, 2021</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="blue"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
